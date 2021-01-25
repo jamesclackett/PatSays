e27dbb65d8ae78de9b0aedcc8b376625f57c707e
James Clackett <jamesclackett@gmail.com>
2021-01-25 10:55:40 +0000

    Build Card and Deck classes

--- app/src/main/java/com/jimboidin/patsays/Game/Card.java
+++ app/src/main/java/com/jimboidin/patsays/Game/Card.java
@@ -1,4 +1,23 @@
 package com.jimboidin.patsays.Game;
 
+import android.media.Image;
+
 public class Card {
+    private String suit;
+    private int value;
+    private boolean isFaceDown = false;
+    private Image icon;
+
+    public Card(String suit, int value, Image icon){
+        this.suit = suit;
+        this.value = value;
+        this.icon = icon;
+    }
+
+    public String getSuit(){return suit; }
+    public int getValue() { return value; }
+    public boolean isFaceDown() {return isFaceDown; }
+    public Image getIcon() {return icon; }
+
+    public void setFaceDown(boolean faceDown) {isFaceDown = faceDown; }
 }
--- app/src/main/java/com/jimboidin/patsays/Game/Deck.java
+++ app/src/main/java/com/jimboidin/patsays/Game/Deck.java
@@ -1,4 +1,60 @@
 package com.jimboidin.patsays.Game;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
 public class Deck {
+    private HashMap<String, ArrayList<Card>> cardMap;
+
+    public Deck() {
+        populateDeck();
+    }
+
+
+    public HashMap<String, ArrayList<Card>> getCardMap() { return cardMap; }
+    public int getSize() { return calculateSize(); }
+
+    private void populateDeck() {
+        //add all cards
+    }
+
+    public void removeJokers(){
+        cardMap.remove("joker");
+    }
+
+    private int calculateSize() {
+        int counter = 0;
+        for (String key : cardMap.keySet()){
+            counter += cardMap.get(key).size();
+        }
+        return counter;
+    }
+
+    public Card drawCard(){
+        //get random suit
+        Random random = new Random();
+        List<String> keys = new ArrayList<>(cardMap.keySet());
+        String randomKey = keys.get(random.nextInt(keys.size()));
+        ArrayList<Card> randomSuit = cardMap.get(randomKey);
+
+        //get random card
+        random = new Random();
+        int randomInt = random.nextInt(randomSuit.size());
+        randomSuit.remove(randomInt);
+
+        return randomSuit.remove(randomInt);
+
+    }
+
+    public ArrayList<Card> dealCards(int players){
+        ArrayList<Card> dealtCardList = new ArrayList<>();
+        int dealSize = calculateSize() / players;
+        for (int i = 0; i < dealSize; i++){
+            dealtCardList.add(drawCard());
+        }
+        return dealtCardList;
+    }
 }
